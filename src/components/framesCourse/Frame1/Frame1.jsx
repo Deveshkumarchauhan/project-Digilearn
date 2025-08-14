774017a29b016ad535daba504959a7b8fa68afa9
Devesh kumar chauhan <deveshchauhan3859@gmail.com>
2025-08-15 00:20:26 +0530

    course change

--- src/components/framesCourse/Frame1/Frame1.jsx
+++ src/components/framesCourse/Frame1/Frame1.jsx
@@ -1,34 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Frame1.css";
-import { FaBook, FaProjectDiagram, FaVideo } from "react-icons/fa";
-import programmingone from "../../../assets img/programmingone.svg"
+import { FaBook, FaDollarSign, FaVideo, FaChevronDown } from "react-icons/fa";
+import programmingone from "../../../assets img/programmingone.svg";
+
 export default function Frame1() {
+  const [showVideos, setShowVideos] = useState(false);
+  const [showFreetext, setShowFreetext] = useState(false);
+
   const videos = [
     { title: "Introduction to Programming", time: "02 : 10" },
     { title: "Understanding Syntax", time: "02 : 50" },
-    { title: "Introduction to Programming", time: "02 : 10" },
-    { title: "Introduction to Programming", time: "02 : 10" },
-    { title: "Introduction to Programming", time: "02 : 10" },
-    { title: "Introduction to Programming", time: "02 : 10" },
-    { title: "Introduction to Programming", time: "02 : 10" },
+    { title: "Variables and Data Types", time: "03 : 20" },
+    { title: "Control Structures", time: "04 : 00" },
+    { title: "Functions in Depth", time: "05 : 10" },
+    { title: "Object-Oriented Basics", time: "06 : 15" },
+    { title: "Project Demo", time: "07 : 40" },
   ];
+
+  const freeText = [...videos];
 
   return (
     <div className="f1-main">
       {/* Left Side */}
       <div className="f1-left">
-        <img
-          src={programmingone}
-          alt="Course"
-          className="f1-course-img"
-        />
+        <img src={programmingone} alt="Course" className="f1-course-img" />
 
         <h2 className="f1-title">Course Details</h2>
 
         <div className="f1-info">
-          <p><FaBook /> 6 courses materials</p>
-          <p><FaProjectDiagram /> 2 Projects</p>
-          <p><FaVideo /> 10 Videos</p>
+          <p>
+            <FaBook /> 6 course materials
+          </p>
+          <p>
+            <FaVideo /> 10 Videos
+          </p>
+          <p>
+            <FaDollarSign /> 2.7 prize
+          </p>
         </div>
 
         <button className="f1-btn">ENROLL NOW</button>
@@ -37,21 +45,66 @@ export default function Frame1() {
       {/* Right Side */}
       <div className="f1-right">
         <p className="f1-desc">
-          Understanding Core Programming Concepts Is Necessary For Starting A<br/>
-          Career In Software Development, Data Science And Many More. This Is<br/>
+          Understanding Core Programming Concepts Is Necessary For Starting A
+          <br />
+          Career In Software Development, Data Science And Many More. This Is
+          <br />
           Your First Step To Becoming A World-class Tech Pro
         </p>
 
-        <h3 className="f1-videos-title"><FaVideo /> Videos</h3>
-
-        <div className="f1-videos-list">
-          {videos.map((video, index) => (
-            <div key={index} className="f1-video-item">
-              <span>{video.title}</span>
-              <span>{video.time}</span>
-            </div>
-          ))}
+        {/* Video Input */}
+        <div className="f1-input-wrapper" style={{ marginLeft: "140px" }}>
+          <FaVideo className="video-icon" />
+          <input
+            type="text"
+            className="video-input"
+            value="Videos"
+            readOnly
+            onClick={() => setShowVideos(!showVideos)}
+          />
+          <FaChevronDown
+            className={`arrow-icon ${showVideos ? "rotate" : ""}`}
+          />
         </div>
+
+        {/* Free Text Input */}
+        <div className="f1-input-wrapper" style={{ marginLeft: "140px" }}>
+          <FaVideo className="video-icon" />
+          <input
+            type="text"
+            className="video-input"
+            value="Free Text"
+            readOnly
+            onClick={() => setShowFreetext(!showFreetext)}
+          />
+          <FaChevronDown
+            className={`arrow-icon ${showFreetext ? "rotate" : ""}`}
+          />
+        </div>
+
+        {/* Dropdown list - Videos */}
+        {showVideos && (
+          <div className="f1-videos-list" style={{ marginLeft: "140px" }}>
+            {videos.map((video, index) => (
+              <div key={index} className="f1-video-item">
+                <span>{video.title}</span>
+                <span>{video.time}</span>
+              </div>
+            ))}
+          </div>
+        )}
+
+        {/* Dropdown list - Free Text */}
+        {showFreetext && (
+          <div className="f1-videos-list" style={{ marginLeft: "140px" }}>
+            {freeText.map((video, index) => (
+              <div key={index} className="f1-video-item">
+                <span>{video.title}</span>
+                <span>{video.time}</span>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
